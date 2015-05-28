1cf0f2757d6989c1299e6367426bc937e88e9c3c
Max Alcala <malcala@malcala-mbp.local>
2015-05-28 10:01:24 -0500

    Add basic brew script

---
+++ devbox_setup.sh
@@ -0,0 +1,9 @@
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap caskroom/cask
+brew install brew-cask
+brew install git
+brev install vim
+brew install emacs
+brew install rbenv
+brew install ruby-build
+rbenv install 2.2.0
