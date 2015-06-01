4b0aefad55571bcb53d942855b3a4117f76ae926
Max Alcala <maxarturo@gmail.com>
2015-06-01 09:08:17 -0500

    Add rbenv gemset for managing project specific gemsets

--- devbox_setup.sh
+++ devbox_setup.sh
@@ -4,6 +4,15 @@ brew install brew-cask
 brew install git
 brev install vim
 brew install emacs
+brew install mysql
 brew install rbenv
 brew install ruby-build
+brew install rbenv-gemset
 rbenv install 2.2.0
+
+# vagrant setup commands
+brew cask install virtualbox
+brew cask install vagrant
+brew cask install vagrant-manager
+vagrant plugin install vagrant-vbguest
+vagrant plugin install vagrant-librarian-chef-nochef
