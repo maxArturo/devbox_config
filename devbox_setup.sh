9dbc5c621c8f87b23288e5047273ceb91980a26e
Max Arturo AS <maxarturo@gmail.com>
2015-09-18 11:13:34 -0500

    Add iterm2 to devbox_setup

--- devbox_setup.sh
+++ devbox_setup.sh
@@ -3,16 +3,15 @@ brew tap caskroom/cask
 brew install brew-cask
 brew install git
 brev install vim
-brew install emacs
 brew install mysql
 brew install rbenv
 brew install ruby-build
 brew install rbenv-gemset
-rbenv install 2.2.0
+rbenv install 2.2.3
+brew cask install iterm2
 
 # vagrant setup commands
 brew cask install virtualbox
 brew cask install vagrant
 brew cask install vagrant-manager
 vagrant plugin install vagrant-vbguest
-vagrant plugin install vagrant-librarian-chef-nochef
